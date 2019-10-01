fd15296a0071fa53a591f80ea1eb1cfd42fb4f76
jerrykingxyz <jerrykingxyz@gmail.com>
2019-10-01 23:03:37 +0800

    feat: update project build command

--- rollup.config.js
+++ rollup.config.js
@@ -1,14 +1,23 @@
 import babel from 'rollup-plugin-babel'
+import { uglify } from 'rollup-plugin-uglify'
+
+const plugins = [
+  babel({
+    exclude: 'node_modules/**',
+  }),
+]
+
+const needMinify = process.env['ENV'] === 'prod'
+const ext = needMinify ? '.min.js' : '.js'
+if (needMinify) {
+  plugins.push(uglify())
+}
 
 export default {
   input: 'src/index.js',
   output: {
-    file: 'dist/urlchangeevent.js',
+    file: `dist/urlchangeevent${ext}`,
     format: 'cjs',
   },
-  plugins: [
-    babel({
-      exclude: 'node_modules/**',
-    }),
-  ],
+  plugins,
 }
