7976157bea691ef1298f6eac6bdb48f4ba4e3ade
jerrykingxyz <jerrykingxyz@gmail.com>
2023-11-10 14:50:52 +0800

    release 0.1.8-beta.1

--- dist/url-change-event.js
+++ dist/url-change-event.js
@@ -53,7 +53,7 @@ let cacheIndex;
 function initState() {
   const state = window.history.state;
   if (!state || typeof state._index !== 'number') {
-    originReplaceState({ _index: window.history.length, ...state }, null, null);
+    originReplaceState({ _index: window.history.length, ...state }, '');
   }
 }
 
